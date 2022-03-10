97ae23c08b3b1e221f4628eeb12bbdc80f9fbbe5
IncognitoUser84 <89994947+SiyangLiu-CTD@users.noreply.github.com>
2022-03-10 00:02:59 -0700

    s

---
+++ main.js
@@ -0,0 +1,7 @@
+getElementById('m_over').addEventListener("mouseover", function(){
+        this.style.color = "red";
+    });
+
+    getElementById('m_over').addEventListener("mouseout", function(){
+        this.style.color = "default";
+    }); 
